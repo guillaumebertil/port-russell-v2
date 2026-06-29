1554f5b57c317621b2be7a3c3d93325770376925
guillaumebertil <guillaume.bertil@gmail.com>
2026-06-29 09:16:46 +0200

    feat: protection des routes login et logout

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,9 @@ Route::middleware('auth')->group(function () {
     });
 });
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
