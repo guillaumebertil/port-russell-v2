fa114dea750e5f3945803848ac23aa31df64076f
guillaumebertil <guillaume.bertil@gmail.com>
2026-06-30 09:58:41 +0200

    style: pages reservations

--- resources/views/catway/show.blade.php
+++ resources/views/catway/show.blade.php
@@ -2,8 +2,8 @@
     <div class="card bg-base-100/90 w-lg shadow-sm">
         <div class="card-body flex justify-center items-center">
             <h1 class="card-title text-4xl">Catway n° {{ $catway->catwayNumber }}</h1>
-            <p><span class="text-2xl font-semibold">Type:</span> {{ $catway->catwayType }}</p>
-            <p><span class="text-2xl font-semibold">Etat:</span> {{ $catway->catwayState }}</p>
+            <p><span class="text-xl font-semibold">Type:</span> {{ $catway->catwayType }}</p>
+            <p><span class="text-xl font-semibold">Etat:</span> {{ $catway->catwayState }}</p>
             @foreach ($reservations as $reservation)
                 <p>{{ $reservation->boatName }}</p>
             @endforeach
@@ -17,18 +17,4 @@
             </div>
         </div>
     </div>
-    {{-- <div>
-        <h2>Numéro: {{ $catway->catwayNumber }}</h2>
-        <p>Type: {{ $catway->catwayType }}</p>
-        <p>Etat: {{ $catway->catwayState }}</p>
-        @foreach ($reservations as $reservation)
-            <p>{{ $reservation->boatName }}</p>
-        @endforeach
-        <a href="/catways/edit/{{ $catway->id }}">Modifier</a>
-        <form action="/catways/{{ $catway->id }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-error">Supprimer</button>
-        </form>
-    </div> --}}
 </x-layout>
--- resources/views/reservation/show.blade.php
+++ resources/views/reservation/show.blade.php
@@ -1,17 +1,37 @@
 <x-layout>
-    <div>
-        <h2>Numéro de la réservation: {{ $reservation->id }}</h2>
-        <p>Numéro du catway: {{ $reservation->catwayNumber }}</p>
-        <p>Nom du client: {{ $reservation->clientName }}</p>
-        <p>Nom du bateau: {{ $reservation->boatName }}</p>
-        <p>Date de début: {{ $reservation->startDate->format('d/m/Y') }}</p>
-        <p>Date de fin: {{ $reservation->endDate->format('d/m/Y') }}</p>
-        
-        <a href="/reservations/edit/{{ $reservation->id }}">Modifier</a>
-        <form action="/reservations/{{ $reservation->id }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-error">Supprimer</button>
-        </form>
+    <div class="card bg-base-100/90 w-lg shadow-sm">
+        <div class="card-body flex justify-center items-center">
+            <h1 class="card-title text-4xl">Reservation n° {{ $reservation->id }}</h1>
+            <p><span class="text-xl font-semibold">Nom du client:</span> {{ $reservation->clientName }}</p>
+            <p><span class="text-xl font-semibold">Nom du bateau:</span> {{ $reservation->boatName }}</p>
+            <p><span class="text-xl font-semibold">Date de départ:</span> {{ $reservation->startDate->format('d/m/Y') }}</p>
+            <p><span class="text-xl font-semibold">Date de d'arrivée:</span> {{ $reservation->endDate->format('d/m/Y') }}</p>
+            <p><span class="text-xl font-semibold">Date de création:</span> {{ $reservation->created_at->format('d/m/Y') }}</p>
+            <p><span class="text-xl font-semibold">Date de modification:</span> {{ $reservation->updated_at->format('d/m/Y') }}</p>
+            <div class="flex w-full justify-center gap-x-8">
+                <a href="/reservations/edit/{{ $reservation->id }}" class="btn btn-success w-[1/2]">Modifier</a>
+                <form action="/reservations/{{ $reservation->id }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-error w-[1/2]">Supprimer</button>
+                </form>
+            </div>
+        </div>
     </div>
 </x-layout>
+
+{{-- <div>
+    <h2>Numéro de la réservation: {{ $reservation->id }}</h2>
+    <p>Numéro du reservation: {{ $reservation->reservationNumber }}</p>
+    <p>Nom du client: {{ $reservation->clientName }}</p>
+    <p>Nom du bateau: {{ $reservation->boatName }}</p>
+    <p>Date de début: {{ $reservation->startDate->format('d/m/Y') }}</p>
+    <p>Date de fin: {{ $reservation->endDate->format('d/m/Y') }}</p>
+    
+    <a href="/reservations/edit/{{ $reservation->id }}">Modifier</a>
+    <form action="/reservations/{{ $reservation->id }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-error">Supprimer</button>
+    </form>
+</div> --}}
